70df1751b3a8f69603fb69f659d430bcf7a00b75
Mathieu Cadet <mathieu@zaft.fr>
2023-12-04 13:39:44 +1100

    Reformating

--- 1.fsx
+++ 1.fsx
@@ -1,28 +1,28 @@
 let parseInt c =
-    try
-        Some(int c)
-    with :? System.FormatException ->
-        None
+  try
+    Some(int c)
+  with :? System.FormatException ->
+    None
 
 "1.input.txt"
 |> System.IO.File.ReadAllLines
 |> Array.map (fun l ->
-    l.ToCharArray()
-    |> Array.choose (fun c -> c.ToString() |> parseInt)
-    |> fun a -> a[0] * 10 + a[a.Length - 1])
+  l.ToCharArray()
+  |> Array.choose (fun c -> c.ToString() |> parseInt)
+  |> fun a -> a[0] * 10 + a[a.Length - 1])
 |> Array.sum // Part 1
 
 let validDigits =
-    [| "zero"
-       "one"
-       "two"
-       "three"
-       "four"
-       "five"
-       "six"
-       "seven"
-       "eight"
-       "nine" |]
+  [| "zero"
+     "one"
+     "two"
+     "three"
+     "four"
+     "five"
+     "six"
+     "seven"
+     "eight"
+     "nine" |]
 
 // Edge case warning
 // 4nine7oneighthm -> 48
@@ -31,26 +31,26 @@ let validDigits =
 "1.input.txt"
 |> System.IO.File.ReadAllLines
 |> Array.map (fun l ->
-    let mutable pos = 0
-    let mutable v = []
+  let mutable pos = 0
+  let mutable v = []
 
-    while pos < l.Length do
-        match parseInt (l[pos].ToString()) with
-        | Some i ->
-            v <- i :: v
-            pos <- pos + 1
-        | None ->
-            let mutable newPos = 0
+  while pos < l.Length do
+    match parseInt (l[pos].ToString()) with
+    | Some i ->
+      v <- i :: v
+      pos <- pos + 1
+    | None ->
+      let mutable newPos = 0
 
-            validDigits
-            |> Array.iteri (fun idx digit ->
-                if newPos = 0 then
-                    if (l[pos..].StartsWith(digit)) then
-                        v <- idx :: v
-                        newPos <- pos + 1)
+      validDigits
+      |> Array.iteri (fun idx digit ->
+        if newPos = 0 then
+          if (l[pos..].StartsWith(digit)) then
+            v <- idx :: v
+            newPos <- pos + 1)
 
-            if newPos > pos then pos <- newPos else pos <- pos + 1
+      if newPos > pos then pos <- newPos else pos <- pos + 1
 
-    v <- v |> List.rev
-    v |> List.toArray |> (fun a -> a[0] * 10 + a[a.Length - 1]))
+  v <- v |> List.rev
+  v |> List.toArray |> (fun a -> a[0] * 10 + a[a.Length - 1]))
 |> Array.sum // Part2
--- 2.fsx
+++ 2.fsx
@@ -1,17 +1,17 @@
 open System.Text.RegularExpressions
 
 let getMaxVals s =
-    let bgr = [| 0; 0; 0 |]
+  let bgr = [| 0; 0; 0 |]
 
-    [| @"(\d+) blue"; @"(\d+) green"; @"(\d+) red" |]
-    |> Array.iteri (fun i p ->
-        for m in Regex.Matches(s, p, RegexOptions.None) do
-            let v = int m.Groups[1].Value
+  [| @"(\d+) blue"; @"(\d+) green"; @"(\d+) red" |]
+  |> Array.iteri (fun i p ->
+    for m in Regex.Matches(s, p, RegexOptions.None) do
+      let v = int m.Groups[1].Value
 
-            if v > bgr[i] then
-                bgr[i] <- v)
+      if v > bgr[i] then
+        bgr[i] <- v)
 
-    bgr
+  bgr
 
 let cond = [| 14; 13; 12 |]
 
@@ -19,10 +19,10 @@ let maxVals = "2.input.txt" |> System.IO.File.ReadAllLines |> Array.map getMaxVa
 
 maxVals
 |> Array.mapi (fun idx vals ->
-    if vals[0] <= cond[0] && vals[1] <= cond[1] && vals[2] <= cond[2] then
-        idx + 1
-    else
-        0)
+  if vals[0] <= cond[0] && vals[1] <= cond[1] && vals[2] <= cond[2] then
+    idx + 1
+  else
+    0)
 |> Array.sum // part 1
 
 maxVals |> Array.map (fun vals -> vals[0] * vals[1] * vals[2]) |> Array.sum // part 2
